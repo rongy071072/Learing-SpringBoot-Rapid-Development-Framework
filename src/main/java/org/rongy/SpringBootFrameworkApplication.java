d5ea02450b29e28d075cecf648044b9f97bd6c75
rongy <108705044+rongy071072@users.noreply.github.com>
2024-12-18 14:24:10 +0800

    first-commit

---
+++ src/main/java/org/rongy/SpringBootFrameworkApplication.java
@@ -0,0 +1,13 @@
+package org.rongy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rongy/SpringBootFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rongy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
